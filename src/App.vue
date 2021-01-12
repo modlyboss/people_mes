decc063ac26adeeab5a144dd4597973cb29254cb
modlyboss <512709877@qq.com>
2021-01-12 17:40:29 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      122222222222222222222222
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">首页</router-link> |
+      <router-link to="/about">别点</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: "app"
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -17,14 +21,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
